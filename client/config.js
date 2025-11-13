313ce9421380071b6da89a78a98e84643f3fae88
jeanlucba <jeanlucbarbierjobs@gmail.com>
2025-11-13 15:07:33 -0500

    push 1

---
+++ client/config.js
@@ -0,0 +1,4 @@
+
+export const marketplaceAddress = "0x0Fc1164392085534Ec1Aa1FF030AF7D1349d0EE3";
+export const transactionsAddress = "0xA44CC65c8B88cAACfAECa1D41cb1acb994aa9e12";
+
---
+++ smart_contract/config.js
@@ -0,0 +1,3 @@
+
+  export const NFTmarketplaceAddress = "0x0Fc1164392085534Ec1Aa1FF030AF7D1349d0EE3"
+  
